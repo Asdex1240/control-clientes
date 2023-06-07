cab64575bc543ac314049896b76cc2d8a96db5ef
Asdex1240 <jose.morenobrn@uanl.edu.mx>
2023-06-07 13:47:32 -0500

    Subimos el proyecto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'control-clientes-2012b',
+    appId: '1:143499606316:web:4c0005f0fd21ca804f1489',
+    storageBucket: 'control-clientes-2012b.appspot.com',
+    apiKey: 'AIzaSyAtdrm4z7PmtZrPRbAv2DZUKWcRX9XPnIY',
+    authDomain: 'control-clientes-2012b.firebaseapp.com',
+    messagingSenderId: '143499606316',
+    measurementId: 'G-D5NMCJV3R0',
+  },
   production: false
 };
 
